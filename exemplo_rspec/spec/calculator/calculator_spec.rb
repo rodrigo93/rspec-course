bd380a70201806d150a038ee63a954910765e740
Rodrigo Marques <rodrigomarques1993@gmail.com>
2018-05-14 07:49:18 -0300

    Using subject

--- exemplo_rspec/spec/calculator/calculator_spec.rb
+++ exemplo_rspec/spec/calculator/calculator_spec.rb
@@ -1,22 +1,21 @@
-require 'calculator'
+require 'calculator' # This is the subject
 
 describe Calculator do
+	subject(:calc) { described_class.new() }
+
 	context '#sum' do
 	  it 'with positive numbers' do
-	  	calculator = Calculator.new
-	  	result = calculator.sum(2,3)
+	  	result = calc.sum(2,3)
 	  	expect(result).to equal(2+3)
 	  end
 
 	  it 'with numbers and one as negative' do
-	  	calculator = Calculator.new
-	  	result = calculator.sum(-2,3)
+	  	result = calc.sum(-2,3)
 	  	expect(result).to equal(-2+3)
 	  end
 
 	  it 'with negative numbers' do
-			calculator = Calculator.new
-	  	result = calculator.sum(-2,-3)
+	  	result = calc.sum(-2,-3)
 	  	expect(result).to equal(-2+(-3))
 	  end
 	end
