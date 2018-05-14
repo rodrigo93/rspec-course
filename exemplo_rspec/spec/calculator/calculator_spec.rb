53733bdc150dc2fa67b3f11bf54c8784226ffafe
Rodrigo Marques <rodrigomarques1993@gmail.com>
2018-05-14 07:52:15 -0300

    Another way of using subject

--- exemplo_rspec/spec/calculator/calculator_spec.rb
+++ exemplo_rspec/spec/calculator/calculator_spec.rb
@@ -1,21 +1,19 @@
 require 'calculator' # This is the subject
 
 describe Calculator do
-	subject(:calc) { described_class.new() }
-
 	context '#sum' do
 	  it 'with positive numbers' do
-	  	result = calc.sum(2,3)
+	  	result = subject.sum(2,3)
 	  	expect(result).to equal(2+3)
 	  end
 
 	  it 'with numbers and one as negative' do
-	  	result = calc.sum(-2,3)
+	  	result = subject.sum(-2,3)
 	  	expect(result).to equal(-2+3)
 	  end
 
 	  it 'with negative numbers' do
-	  	result = calc.sum(-2,-3)
+	  	result = subject.sum(-2,-3)
 	  	expect(result).to equal(-2+(-3))
 	  end
 	end
