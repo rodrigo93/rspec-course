dbcc9c42f452385b065e91cda726026fe496e59f
Rodrigo Marques <rodrigomarques1993@gmail.com>
2018-05-15 07:49:07 -0300

    Matchers

--- exemplo_rspec/spec/calculator/calculator_spec.rb
+++ exemplo_rspec/spec/calculator/calculator_spec.rb
@@ -2,26 +2,27 @@ require 'calculator' # This is the subject
 
 describe Calculator, "sobre a Calculadora" do
 
-	# subject(:calc) { described_class.new() } # Explicit subject
+  # subject(:calc) { described_class.new() } # Explicit subject
 
-	context '#sum' do
-	  it 'with positive numbers' do
-	  	result = subject.sum(2,3)
-	  	expect(result).to equal(2+3)
-	  end
+  context '#sum' do
+    it 'with positive numbers' do
+      result = subject.sum(2,3)
+      expect(result).to eq(2+3)
+      expect(result).not_to eq(6)  
+    end
 
-	  it 'with numbers and one as negative' do
-	  	result = subject.sum(-2,3)
-	  	expect(result).to equal(-2+3)
-	  end
+    it 'with numbers and one as negative' do
+      result = subject.sum(-2,3)
+      expect(result).to eq(-2+3)
+    end
 
-	  it 'with negative numbers' do
-	  	result = subject.sum(-2,-3)
-	  	expect(result).to equal(-2+(-3))
-	  end
+    it 'with negative numbers' do
+      result = subject.sum(-2,-3)
+      expect(result).to eq(-2+(-3))
+    end
 
-	  xit 'pending test' do
-	  	result.sum(123,321)
-	  end
-	end
+    xit 'pending test' do
+      result.sum(123,321)
+    end
+  end
 end
