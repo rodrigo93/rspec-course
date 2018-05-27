2eef4e7ba65442c9e5477e8cc0dd70c8465d445b
rodrigo93 <rodrigomarques1993@gmail.com>
2018-05-27 12:02:56 -0300

    One liner syntax, is expected

--- exemplo_rspec/spec/matchers/ranges/ranges_spec.rb
+++ exemplo_rspec/spec/matchers/ranges/ranges_spec.rb
@@ -1,7 +1,18 @@
 describe (1..10), 'Ranges' do
   it '#cover' do
-  	expect(subject).to cover(2)
-  	expect(subject).to cover(2,5)
-  	expect(subject).not_to cover(0,11)
+    expect(subject).to cover(2)
+    expect(subject).to cover(2,5)
+    expect(subject).not_to cover(0,11)
   end
+
+  it '#is_expected' do
+    is_expected.to cover(2)
+    is_expected.to cover(2,5)
+    is_expected.not_to cover(0,11)
+  end
+
+  it { is_expected.to cover(2) } # should
+  it { is_expected.to cover(2, 5) } # should cover 2 and 5
+  it { is_expected.not_to cover(11) } # should not
+  it { is_expected.not_to cover(11, 12) } # should not cover 11 and 12
 end
