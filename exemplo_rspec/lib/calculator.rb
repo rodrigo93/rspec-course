3fe932300f739d42346f0ec73e0b9f6bb1f10e0e
rodrigo93 <rodrigomarques1993@gmail.com>
2018-05-27 11:34:29 -0300

    Matcher raise error

--- exemplo_rspec/lib/calculator.rb
+++ exemplo_rspec/lib/calculator.rb
@@ -1,5 +1,9 @@
 class Calculator
-	def sum(first_number, second_number)
-		first_number + second_number
-	end
+  def sum(first_number, second_number)
+    first_number + second_number
+  end
+
+  def div(first_number, second_number)
+    first_number/second_number
+  end
 end
--- exemplo_rspec/spec/calculator/calculator_spec.rb
+++ exemplo_rspec/spec/calculator/calculator_spec.rb
@@ -3,6 +3,14 @@ require 'calculator' # This is the subject
 describe Calculator, "sobre a Calculadora" do
 
   # subject(:calc) { described_class.new() } # Explicit subject
+  context '#div' do
+    it 'divided by zero' do
+      expect{subject.div(3,0)}.to raise_error(ZeroDivisionError)
+      expect{subject.div(3,0)}.to raise_error("divided by 0")
+      expect{subject.div(3,0)}.to raise_error(ZeroDivisionError, "divided by 0")
+      expect{subject.div(3,0)}.to raise_error(/divided/) # RegEx
+    end
+  end
 
   context '#sum' do
     it 'with positive numbers' do
